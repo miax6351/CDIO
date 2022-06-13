5330a1a0d5735b239cc15168aa4370e795286084
TheZoap <lucas.holstein@gmail.com>
2022-06-13 11:45:34 +0200

    BIG INSANE UI CHANGES - WILL CHANGE YOUR LIFE AND GIVE YOU A BILLION DOLLARS CAP

--- app/src/main/java/org/tensorflow/lite/examples/detection/CameraActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/CameraActivity.java
@@ -243,18 +243,18 @@ public abstract class CameraActivity extends AppCompatActivity
                 break;
               case BottomSheetBehavior.STATE_EXPANDED:
                 {
-                  bottomSheetArrowImageView.setImageResource(R.drawable.icn_chevron_down);
+                  bottomSheetArrowImageView.setImageResource(R.drawable.icn_chevron_down_black);
                 }
                 break;
               case BottomSheetBehavior.STATE_COLLAPSED:
                 {
-                  bottomSheetArrowImageView.setImageResource(R.drawable.icn_chevron_up);
+                  bottomSheetArrowImageView.setImageResource(R.drawable.icn_chevron_up_black);
                 }
                 break;
               case BottomSheetBehavior.STATE_DRAGGING:
                 break;
               case BottomSheetBehavior.STATE_SETTLING:
-                bottomSheetArrowImageView.setImageResource(R.drawable.icn_chevron_up);
+                bottomSheetArrowImageView.setImageResource(R.drawable.icn_chevron_up_black);
                 break;
             }
           }
